aeabe08b986d424a3cfac526d93c5c864e2f3c73
atriple <andiaqil.dpe@gmail.com>
2019-08-12 18:51:47 +0700

    Delete LoginPage to fix filename capitalization

--- src/components/LoginPage/LoginPage.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import  { LogInPage } from './LogInPage';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<LogInPage />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
