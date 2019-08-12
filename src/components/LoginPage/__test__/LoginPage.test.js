541ce4e182416bf819079094885ccffbd2e80085
seagalputra <seagalputra@gmail.com>
2019-08-12 17:42:14 +0700

    [dio] fix rendering component to make test case pass

--- src/components/LoginPage/__test__/LoginPage.test.js
+++ src/components/LoginPage/__test__/LoginPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoginPage from './LoginPage';
+import LoginPage from '..';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
