5c557c9196a07a099581ddb20cd9fcc979cbd525
atriple <andiaqil.dpe@gmail.com>
2019-08-12 18:43:05 +0700

    Login Page progress

--- src/components/LoginPage/LoginPage.test.js
+++ src/components/LoginPage/LoginPage.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoginPage from './LoginPage';
+import  { LogInPage } from './LogInPage';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<LoginPage />, div);
+  ReactDOM.render(<LogInPage />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
