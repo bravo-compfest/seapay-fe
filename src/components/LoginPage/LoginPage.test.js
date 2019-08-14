9a85a38000931fcf89f431ea893b2403deef15a8
seagalputra <seagalputra@gmail.com>
2019-08-14 12:14:13 +0700

    [dio] modify Signin test case

--- src/components/LoginPage/LoginPage.test.js
+++ src/components/LoginPage/LoginPage.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LogInPage from './LogInPage';
+import LoginPage from './LoginPage';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<LogInPage />, div);
+  ReactDOM.render(<LoginPage />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
