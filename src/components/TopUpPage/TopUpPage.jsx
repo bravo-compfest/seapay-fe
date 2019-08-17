c27531ae50875f8e8685eb7c5be47370ca5690d6
atriple <andiaqil.dpe@gmail.com>
2019-08-18 06:30:08 +0700

    Update TopUpPage

--- src/components/TopUpPage/TopUpPage.jsx
+++ src/components/TopUpPage/TopUpPage.jsx
@@ -4,11 +4,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
   paper: {
     padding: theme.spacing(2),
+    margin: 'auto'
   },
 }));
 
@@ -16,33 +16,27 @@ function TopUpPage() {
   const classes = useStyles();
 
   return (
-    <div>
-      <Grid container spacing={3}>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
-            <Typography variant="h5">
-              Top Up Balance
-            </Typography>
-            <TextField
-              id="outlined-number"
-              label="Nominal"
-              value="0"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              margin="normal"
-              variant="outlined"
-            />
-            <Grid container justify="flex-start">
-              <Button variant="contained" spacing={3} color="primary">
-                Top Up
-              </Button>
-            </Grid>
-          </Paper>
-        </Grid>
-      </Grid>
-    </div>
+      <Paper className={classes.paper}>
+        <Typography variant="h5">
+          Top Up Balance
+        </Typography>
+        {/* TODO: Add form and api request */}
+        <TextField
+          id="outlined-number"
+          label="Nominal"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          margin="normal"
+          variant="outlined"
+        />
+        <div>
+          <Button variant="contained" color="primary">
+            Top Up
+          </Button>
+        </div>
+      </Paper>
   );
 }
 
