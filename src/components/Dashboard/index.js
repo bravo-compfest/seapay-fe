0194a4c0b6e3949ffda5a43d9b5d1124d1b8be71
atriple <andiaqil.dpe@gmail.com>
2019-08-18 06:04:36 +0700

    Update dashboard

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { authenticationService } from '../../services';
 
 import NavBar from '../NavBar';
+import CustomerCard from '../CustomerCard';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 class Dashboard extends React.Component {
   constructor(props){
@@ -22,6 +25,15 @@ class Dashboard extends React.Component {
       <div>
         <NavBar />
         <h1> Login Success </h1>
+        <Grid container spacing={2}>
+          <Grid item xs>
+            {/* TODO: Insert props with name and balance based on user data */}
+            <CustomerCard />
+          </Grid>
+          <Grid item xs={6}>
+            <Paper></Paper>
+          </Grid>
+        </Grid>
       </div>
     );
   }
